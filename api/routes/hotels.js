c0f1d14284041109a3c951e288669b996894bc18
ofovweewere <ewereeman@gmail.com>
2023-01-02 12:41:32 -0500

    Node MongoDB Model Relations

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -6,17 +6,18 @@ import {
   getHotels,
   updateHotel,
 } from "../controllers/hotel.js";
+import { verifyAdmin } from "../utils/verifyToken.js";
 import { createError } from "../utils/error.js";
 const router = express.Router();
 export default router;
 import Hotel from "../models/Hotel.js";
 //CREATE
-router.post("/", createHotel);
+router.post("/", verifyAdmin, createHotel);
 //UPDATE
-router.put("/:id", updateHotel);
+router.put("/:id", verifyAdmin, updateHotel);
 //DELETE
-router.delete("/:id", deleteHotel);
+router.delete("/:id", verifyAdmin, deleteHotel);
 //GET
-router.get("/:id", getHotel);
+router.get("/:id", verifyAdmin, getHotel);
 //GET ALL
 router.get("/", getHotels);
